823702036602067bc787ba5dea8daf0f43d98434
Eric <ea242@njit.edu>
2018-05-01 21:59:59 -0400

    fixed ent_center.py and movie.py to standard

--- ent_center.py
+++ ent_center.py
@@ -1,40 +1,48 @@
 import media
 import fresh_tomatoes
 
+
 # Movie objects for website
 good_will = media.Movie("Good Will Hunting",
-                        "A smart Boston guy who tries to understand relationships",
-                        "http://coolspotters.com/files/photos/369706/good-will-hunting-profile.jpg",
+                        ("A smart Boston guy who "
+                         "tries to understand relationships"),
+                        ("http://coolspotters.com/files"
+                         "/photos/369706/good-will-hunting-profile.jpg"),
                         "https://www.youtube.com/watch?v=ReIJ1lbL-Q8")
 
 forrest_gump = media.Movie("Forrest Gump",
                            "A simple minded guy that goes through life",
-                           "https://image.tmdb.org/t/p/original/yE5d3BUhE8hCnkMUJOo1QDoOGNz.jpg",
+                           ("https://image.tmdb.org/t/p/"
+                            "original/yE5d3BUhE8hCnkMUJOo1QDoOGNz.jpg"),
                            "https://www.youtube.com/watch?v=bLvqoHBptjg")
 
 ben_button = media.Movie("The Curious Case of Benjamin Button",
                          "A old man who dies as a baby",
-                         "https://images-na.ssl-images-amazon.com/images/I/71A8mO-OqBL._SY445_.jpg",
+                         ("https://images-na.ssl-images-amazon.com/"
+                          "images/I/71A8mO-OqBL._SY445_.jpg"),
                          "https://www.youtube.com/watch?v=VqeqaweXBV0")
 
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
-                        "https://vignette.wikia.nocookie.net/disney/images/8/80/Toy_Story_-_Poster.jpg",
+                        ("https://vignette.wikia.nocookie.net/"
+                         "disney/images/8/80/Toy_Story_-_Poster.jpg"),
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
-                     "https://i.pinimg.com/originals/c3/2e/40/c32e40b633ff92a2d3048f5ce8570c90.jpg",
+                     ("https://i.pinimg.com/originals/c3/2e/"
+                      "40/c32e40b633ff92a2d3048f5ce8570c90.jpg"),
                      "https://www.youtube.com/watch?v=d1_JBMrrYw8")
 
 
 inception = media.Movie("Inception",
                         "A group of people in dreams within dreams",
-                        "http://2.bp.blogspot.com/_Iau3R3yMIr4/TMBDGzcvwSI/AAAAAAAACbQ/77grl8TYdK4/s1600/inception1.jpg",
+                        ("http://2.bp.blogspot.com/_Iau3R3yMIr4/TMBDGzcvwSI"
+                         "/AAAAAAAACbQ/77grl8TYdK4/s1600/inception1.jpg"),
                         "https://www.youtube.com/watch?v=66TuSJo4dZM")
 
 # List of movies for
-movies = [good_will,forrest_gump,ben_button,toy_story,avatar,inception]
+movies = [good_will, forrest_gump, ben_button, toy_story, avatar, inception]
 
 # function from fresh_tomatoes to genereate webpage
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,13 +1,24 @@
 import webbrowser
 
+
 class Movie():
-    """This class provides a way to stre movie related information"""
+    """
+    This class provides a way to store movie related information.
+    The constructor takes four arguments which contains detail of the Movie.
+    movie_title - title of the Movie.
+    movie_storyline - a description of the Movie.
+    poster_image - URL for a poster image of the Movie.
+    trailer_youtube - URL for a youtube trailer.
+    There is one function called show_tailer() 
+    which opens the URL in trailer_youtube_url.
+    """
 
     # Ratings used for movies
-    VALID_RATINGS = ["G","PG","PG-13","R"]
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
     # Constructor for the Movie class
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
